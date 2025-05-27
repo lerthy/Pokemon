b1c00c918fb2e7ea6b9cf688b779ac59ea1ffd35
lerthy <lerdi.salihi@student.uni-pr.edu>
2025-05-27 13:31:10 +0200

    first commit

---
+++ src/model/pokemon.ts
@@ -0,0 +1,13 @@
+export default interface pokemon {
+   pokemonNumber: number;
+   attack: number; 
+   defense: number;
+   devolution: string;
+   evolution: string;
+   evolutionFamily: string[];
+   healthPoints: number;
+   mainImage: string;
+   pokemonName: string;
+   pokemonType: string[];
+   speed: number;
+}
